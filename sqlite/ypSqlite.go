4c3d6ddd8dddfbd573e8c565bd57a7707696da1f
yangping <YYyangping@outlook.com>
2021-06-06 10:28:51 +0800

    sqlite

--- sqlite/ypSqlite.go
+++ sqlite/ypSqlite.go
@@ -6,51 +6,169 @@ import (
     _ "github.com/mattn/go-sqlite3"
 )
 
+/*
+//@example
+//var my sqlite.SqliteClient
+//my.Open("./test.db")
+*/
 type SqliteClient struct {
 	db *sql.DB
-	isConnect bool
+	IsConnect bool
 }
 
 func (client *SqliteClient) Open(filePath string) {
 	var err error
 	client.db, err = sql.Open("sqlite3", filePath)
     checkErr(err)
+    client.IsConnect = true
 }
 
-func (client *SqliteClient) Select() {
-	rows, err := client.db.Query("SELECT * FROM test")
-    checkErr(err)
-
-    for rows.Next() {
-        var id int
-        var name string
-        err = rows.Scan(&id, &name)
-        checkErr(err)
-        fmt.Println(id,name)
-    }
+func (client *SqliteClient) Close(filePath string) {
+	client.db.Close()
+    client.IsConnect = false
 }
 
+/*
+//@example
+//cloumns := []string{"ID INT PRIMARY KEY NOT NULL","name TEXT NOT NULL"}
+//client.CreateTable("test",cloumns)
+*/
 func (client *SqliteClient) CreateTable(tablename string, columns []string){
 	n := len(columns)
 	//sql := "CREATE TABLE test (ID INT PRIMARY KEY NOT NULL,NAME TEXT NOT NULL)" 
-	sql := "CREATE TABLE " + tablename + " ("
+	sqlStr := "CREATE TABLE " + tablename + " ("
 	for i := 0; i < n; i++ {
-		sql += columns[i]
+		sqlStr += columns[i]
 		if i<n-1 {
-			sql += ","
+			sqlStr += ","
 		}
 	}
-	sql += ")"
-	fmt.Println(sql)
-	res, err := client.db.Exec(sql)
+	sqlStr += ")"
+	_, err := client.db.Exec(sqlStr)
 	checkErr(err)
-	fmt.Println(res)
 }
 
+/*
+//@example
+//client.DeleteTable("hello")
+*/
+func (client *SqliteClient) DeleteTable(tablename string) {
+    //sql := "DROP TABLE test"
+    sqlStr := "DROP TABLE " + tablename
+    _, err := client.db.Exec(sqlStr)
+	checkErr(err)
+}
+
+/*
+//@example
+//values := []string{"111","aaa"}
+//client.Insert("test", values)
+*/
+func (client *SqliteClient) Insert(tablename string, values []string) int64 {
+    //sql := "INSERT INTO test values(?,?)"
+    n := len(values)
+    sqlStr := "INSERT INTO " + tablename + " values("
+    for i := 0; i < n; i++ {
+        sqlStr += "'" + values[i] + "'"
+        if i<n-1 {
+            sqlStr += ","
+        }
+    }
+    sqlStr += ")"
+
+    res, err := client.db.Exec(sqlStr)
+    checkErr(err)
+    if res==nil {
+        return 0
+    }
+    id, err := res.RowsAffected()
+    checkErr(err)
+
+    return id
+}
+
+/*
+//@example
+//aa := client.Select("test", "*", "ID = 111")
+//fmt.Println(aa)
+*/
+func (client *SqliteClient) Select(tablename string, selectColumns string, condition string) []map[string]string {
+    sqlStr := "SELECT " + selectColumns + " FROM " + tablename 
+    if condition!="" {
+        sqlStr += " WHERE " + condition
+    }
+	rows, err := client.db.Query(sqlStr)
+    checkErr(err)
+    if rows==nil {
+        return nil
+    }
+    columns, err := rows.Columns()
+    checkErr(err)
+
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+        scanArgs[i] = &values[i]
+    }
+ 	ccc,err := rows.Columns()
+	var slice []map[string]string
+	for rows.Next(){
+		var myMap map[string]string
+        myMap = make(map[string]string)
+		rows.Scan(scanArgs...)
+		var value string
+        for i , col := range values {
+            if col == nil {
+                value = "NULL"
+            } else {
+                value = string(col)
+				myMap[ccc[i]] = value
+            }
+		}
+		slice = append(slice, myMap)
+	}
+	rows.Close()
+	return slice
+}
+
+/*
+@example
+//aa := client.Delete("test", "ID=111")
+//fmt.Println(aa)
+*/
+func (client *SqliteClient) Delete(tablename string, condition string) int64 {
+    sqlStr := "delete from " + tablename + " where "
+    if condition=="" {
+        return 0
+    } else {
+        sqlStr += condition
+    }
+    res, err := client.db.Exec(sqlStr)
+    checkErr(err)
+    if res==nil {
+        return 0
+    }
+
+    id, err := res.RowsAffected()
+    checkErr(err)
+
+    return id
+}
+
+func checkErr(err error) {
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+
+
+
+/*
 func Test() {
     db, err := sql.Open("sqlite3", "./foo.db")
     checkErr(err)
-	/*
+	
 	//新建数据表
 	sql := "CREATE TABLE test (ID INT PRIMARY KEY NOT NULL,NAME TEXT NOT NULL)" 
 	res, err := db.Exec(sql)
@@ -92,7 +210,7 @@ func Test() {
         err = rows.Scan(&id, &name)
         checkErr(err)
         fmt.Println(id,name)
-    }*/
+    }
 
     //删除数据
     stmt, err := db.Prepare("delete from test where ID=?")
@@ -105,14 +223,9 @@ func Test() {
     checkErr(err)
 
     fmt.Println(affect)
-/*	*/
+/*	
 
     db.Close()
 
-}
+}*/
 
-func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
-}
